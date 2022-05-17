3974c1613134f0d3b492f393c72025a2500c517b
Fabkar <fab_862@hotmail.com>
2022-05-16 23:42:46 -0500

    redisBasic

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,130 +1,97 @@
 #!/usr/bin/env python3
-"""exercise"""
+""" module docs """
 
+
+from functools import wraps
 import redis
 from typing import Union, Optional, Callable
 from uuid import uuid4
-from functools import wraps
 
 
 def count_calls(method: Callable) -> Callable:
-    """[summary]
-    Args:
-        method (Callable): [description]
-    Returns:
-        Callable: [description]
-    """
+    """ method doc """
+
     key = method.__qualname__
 
     @wraps(method)
     def wrapper(self, *args, **kwargs):
+        """ method doc """
         self._redis.incr(key)
         return method(self, *args, **kwargs)
-
     return wrapper
 
 
 def call_history(method: Callable) -> Callable:
-    """[summary]
-    Args:
-        method (Callable): [description]
-    Returns:
-        Callable: [description]
-    """
+    """ method doc """
+
     @wraps(method)
     def wrapper(self, *args, **kwargs):
+        """ method doc """
         input = str(args)
         self._redis.rpush(method.__qualname__ + ":inputs", input)
-
         output = str(method(self, *args, **kwargs))
-        self._redis.rpush(method.__qualname__ + "outputs", output)
-
+        self._redis.rpush(method.__qualname__ + ":outputs", output)
         return output
     return wrapper
 
 
 def replay(fn: Callable):
-    """[summary]
-    Args:
-        fn (Callable): [description]
-    """
+    """ method doc """
     r = redis.Redis()
-    fn_name = fn.__qualname__
-    n_calls = r.get(fn_name)
+    f_name = fn.__qualname__
+    n_calls = r.get(f_name)
     try:
-        n_calls = n_calls.decode("utf-8")
+        n_calls = n_calls.decode('utf-8')
     except Exception:
         n_calls = 0
-    print(f'{fn_name} was called {n_calls} times:')
+    print(f'{f_name} was called {n_calls} times:')
 
-    inputs = r.lrange(fn_name + ":inputs", 0, -1)
-    outputs = r.lrange(fn_name + ":outputs", 0, -1)
+    ins = r.lrange(f_name + ":inputs", 0, -1)
+    outs = r.lrange(f_name + ":outputs", 0, -1)
 
-    for ins, outs in zip(inputs, outputs):
+    for input, output in zip(ins, outs):
         try:
-            ins = ins.decode("utf-8")
+            input = input.decode('utf-8')
         except Exception:
-            ins = ""
+            input = ""
         try:
-            outs = outs.decode("utf-8")
+            output = output.decode('utf-8')
         except Exception:
-            outs = ""
-
-        print(f'{fn_name}(*{ins}) -> {outs}')
+            output = ""
+        print(f'{f_name}(*{input}) -> {output}')
 
 
 class Cache:
+    """ class docs """
 
-    def ___init__(self):
-        """ Create first instace of redis and
-        clean all db with flushdb
-        """
+    def __init__(self) -> None:
+        """ constructor docs """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """ create the first key-value pair into db
-        Args:
-            data (Any): data can be a str, bytes, int or float.
-        Returns:
-            str: Uniq identifier ID
-        """
-        ID = str(uuid4())
-        self._redis.set(ID, data)
-        return ID
+        """ method docs """
+        random_key = str(uuid4())
+        self._redis.set(random_key, data)
+        return random_key
 
-    def get(self, key: str, fn: Optional[callable] = None) \
-            -> Union[str, bytes, int, float]:
-        """ Create first instace of redis and
-        clean all db with flushdb
-        """
-        if fn:
-            v = self._redis.get(key)
-
-        return v.decode("utf-8")
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """ method docs """
+        value = self._redis.get(key)
+        return fn(value) if fn else value
 
     def get_str(self, key: str) -> str:
-        """[summary]
-        Args:
-            key (str): [description]
-        Returns:
-            str: [description]
-        """
-        v = self._redis.get(key)
-        return v.decode("utf-8")
+        """ method docs """
+        return self._redis.get(key).decode("utf-8")
 
     def get_int(self, key: str) -> int:
-        """[summary]
-        Args:
-            key (str): [description]
-        Returns:
-            int: [description]
-        """
-        v = self._redis.get(key)
+        """ method docs """
+        value = self._redis.get(key)
         try:
-            c = int(v.decode("utf-8"))
-        except Exception:
-            v = 0
-        return v
+            value = int(value.decode("utf-8"))
+        except ValueError:
+            value = 0
+        return value
